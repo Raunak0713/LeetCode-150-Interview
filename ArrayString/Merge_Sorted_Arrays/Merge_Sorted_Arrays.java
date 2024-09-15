b897dd6eb6e0ff8d2186dde8d71915fa4e5ba4b9
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-09-15 11:22:12 +0530

    fix

--- ArrayString/Merge_Sorted_Arrays/Merge_Sorted_Arrays.java
+++ ArrayString/Merge_Sorted_Arrays/Merge_Sorted_Arrays.java
@@ -1,7 +1,5 @@
 package ArrayString.Merge_Sorted_Arrays;
 
-import java.util.Arrays;
-
 public class Merge_Sorted_Arrays {
 
   public void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -21,67 +19,5 @@ public class Merge_Sorted_Arrays {
       lastIndex--;
       n--;
     }
-  }
-
-  public static void main(String[] args) {
-    int[] nums1_case1 = {1, 2, 3, 0, 0, 0};
-    int m_case1 = 3;
-    int[] nums2_case1 = {2, 5, 6};
-    int n_case1 = 3;
-    int[] expected_case1 = {1, 2, 2, 3, 5, 6};
-
-    int[] nums1_case2 = {1};
-    int m_case2 = 1;
-    int[] nums2_case2 = {};
-    int n_case2 = 0;
-    int[] expected_case2 = {1};
-
-    int[] nums1_case3 = {0};
-    int m_case3 = 0;
-    int[] nums2_case3 = {1};
-    int n_case3 = 1;
-    int[] expected_case3 = {1};
-
-    Merge_Sorted_Arrays merger = new Merge_Sorted_Arrays();
-
-    System.out.println();
-    merger.merge(nums1_case1, m_case1, nums2_case1, n_case1);
-    {
-      boolean isCorrect = Arrays.equals(nums1_case1, expected_case1);
-      if (isCorrect) {
-        System.out.println("Test Case 1: ✅");
-      } else {
-        System.out.println("Test Case 1: ❌");
-      }
-      System.out.println("Expected: " + Arrays.toString(expected_case1));
-      System.out.println("Actual:   " + Arrays.toString(nums1_case1));
-    }
-    System.out.println();
-
-    merger.merge(nums1_case2, m_case2, nums2_case2, n_case2);
-    {
-      boolean isCorrect = Arrays.equals(nums1_case2, expected_case2);
-      if (isCorrect) {
-        System.out.println("Test Case 2: ✅");
-      } else {
-        System.out.println("Test Case 2: ❌");
-      }
-      System.out.println("Expected: " + Arrays.toString(expected_case2));
-      System.out.println("Actual:   " + Arrays.toString(nums1_case2));
-    }
-    System.out.println();
-
-    merger.merge(nums1_case3, m_case3, nums2_case3, n_case3);
-    {
-      boolean isCorrect = Arrays.equals(nums1_case3, expected_case3);
-      if (isCorrect) {
-        System.out.println("Test Case 3: ✅");
-      } else {
-        System.out.println("Test Case 3: ❌");
-      }
-      System.out.println("Expected: " + Arrays.toString(expected_case3));
-      System.out.println("Actual:   " + Arrays.toString(nums1_case3));
-    }
-    System.out.println();
   }
 }
--- ArrayString/Remove_Element/Remove_Element.java
+++ ArrayString/Remove_Element/Remove_Element.java
@@ -1,7 +1,5 @@
 package ArrayString.Remove_Element;
 
-import java.util.Arrays;
-
 public class Remove_Element {
 
   public int removeElement(int[] nums, int val) {
@@ -14,45 +12,6 @@ public class Remove_Element {
     }
     return k;
   }
-
-  public static void main(String[] args) {
-    int[] nums_case1 = {3, 2, 2, 3};
-    int val_case1 = 3;
-    int[] expected_case1 = {2, 2};
-
-    int[] nums_case2 = {0, 1, 2, 2, 3, 0, 4, 2};
-    int val_case2 = 2;
-    int[] expected_case2 = {0, 1, 4, 0, 3};
-
-    Remove_Element remover = new Remove_Element();
-
-    System.out.println();
-    int k_case1 = remover.removeElement(nums_case1, val_case1);
-    {
-      boolean isCorrect = k_case1 == expected_case1.length &&
-                          Arrays.equals(Arrays.copyOfRange(nums_case1, 0, k_case1), expected_case1);
-      if (isCorrect) {
-        System.out.println("Test Case 1: ✅");
-      } else {
-        System.out.println("Test Case 1: ❌");
-      }
-      System.out.println("Expected: " + Arrays.toString(expected_case1));
-      System.out.println("Actual:   " + Arrays.toString(Arrays.copyOfRange(nums_case1, 0, k_case1)));
-    }
-    System.out.println();
-
-    int k_case2 = remover.removeElement(nums_case2, val_case2);
-    {
-      boolean isCorrect = k_case2 == expected_case2.length &&
-                          Arrays.equals(Arrays.copyOfRange(nums_case2, 0, k_case2), expected_case2);
-      if (isCorrect) {
-        System.out.println("Test Case 2: ✅");
-      } else {
-        System.out.println("Test Case 2: ❌");
-      }
-      System.out.println("Expected: " + Arrays.toString(expected_case2));
-      System.out.println("Actual:   " + Arrays.toString(Arrays.copyOfRange(nums_case2, 0, k_case2)));
-    }
-    System.out.println();
-  }
 }
+
+ 
